cf571c5146091bca778455909c170bd5908e08f1
KsaweryThornton <115893688+KsaweryThornton@users.noreply.github.com>
2023-07-23 18:55:43 +0000

    Deploying to gh-pages from @ KsaweryThornton/goit-react-hw-08-phonebook@52ff85bfc39eacaa33a580e4d94b7615e486b32d 🚀

---
+++ static/js/266.a14c649e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function l(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsxs)("h1",{style:c.title,children:["Hello! It's your contact list!"," "]})})}}}]);
+//# sourceMappingURL=266.a14c649e.chunk.js.map
